25fc1d2e20e73064c81bbcd71eaf10176eccdab0
jaeml06 <jaeml0630@gmail.com>
2025-05-27 13:55:11 +0900

    feat: 펫 정보 수정 로직 변경

--- api/update.ts
+++ api/update.ts
@@ -3,15 +3,23 @@ import { fetchWithAuth } from './util';
 
 export async function updatePetInfo(
   petId: string,
-  updatedData: { name?: string; birthdate?: string; species?: string; gender?: string }
+  updatedData: {
+    name?: string;
+    birthdate?: string;
+    species?: string;
+    gender?: string;
+  }
 ) {
-  const response = await fetchWithAuth(`http://localhost:8081/api/list/${petId}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(updatedData),
-  });
+  const response = await fetchWithAuth(
+    `https://api.saffir.co.kr/pets/${petId}`,
+    {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedData),
+    }
+  );
 
   if (!response.ok) {
     throw new Error('API 업데이트 중 에러 발생');
